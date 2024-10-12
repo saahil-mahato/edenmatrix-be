684949de10b44badf23fccd0f42210c6942d638b
Saahil Mahato <115351000+saahil-mahato@users.noreply.github.com>
2024-10-12 21:18:23 +0545

    refactor: decouple auth module functions and organize into service (#8)

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,18 +1,13 @@
 package controllers
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/saahil-mahato/edenmatrix-be/src/models"
-	"github.com/saahil-mahato/edenmatrix-be/src/repositories"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/saahil-mahato/edenmatrix-be/src/services"
 )
 
 type AuthController struct {
-	Repo repositories.AuthRepository
+	Service services.AuthService
 }
 
 func (ac *AuthController) Register(c *fiber.Ctx) error {
@@ -22,17 +17,7 @@ func (ac *AuthController) Register(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not create user"})
-	}
-	user.Password = string(hashedPassword)
-
-	if err := ac.Repo.CreateUser(user); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not create user"})
-	}
-
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": fmt.Sprintf("Successfully created user %s", user.Username)})
+	return ac.Service.CreateUser(c, user)
 }
 
 func (ac *AuthController) Login(c *fiber.Ctx) error {
@@ -42,37 +27,6 @@ func (ac *AuthController) Login(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
 
-	dbUser, err := ac.Repo.FindUserByEmail(authPayload.Email)
-	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "User is not registered"})
-	}
+	return ac.Service.LoginUser(c, authPayload)
 
-	err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(authPayload.Password))
-	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid credentials"})
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":  dbUser.ID,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	var jwtSecret = []byte("your_secret_key")
-	tokenString, err := token.SignedString(jwtSecret)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not generate token"})
-	}
-
-	cookie := fiber.Cookie{
-		Name:     "jwt",
-		Value:    tokenString,
-		Expires:  time.Now().Add(24 * time.Hour),
-		HTTPOnly: true,
-		Secure:   true,
-		SameSite: fiber.CookieSameSiteStrictMode,
-	}
-
-	c.Cookie(&cookie)
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Login successful"})
 }
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/saahil-mahato/edenmatrix-be/src/controllers"
-	"github.com/saahil-mahato/edenmatrix-be/src/repositories"
+	"github.com/saahil-mahato/edenmatrix-be/src/services"
 )
 
 func Setup(app *fiber.App) {
 	apiVersion := "/v1"
-	authController := controllers.AuthController{Repo: repositories.AuthRepository{}}
+	authController := controllers.AuthController{Service: services.AuthService{}}
 
 	app.Post(fmt.Sprintf("%s/register", apiVersion), authController.Register)
 	app.Post(fmt.Sprintf("%s/login", apiVersion), authController.Login)
